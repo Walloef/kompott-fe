6afe8849059d9251de507c4ad0078a7244b8c0c0
Walloef <wallof.kristofer@gmail.com>
2022-10-22 16:28:13 +0200

    init

---
+++ src/Games/ReactGame/index.tsx
@@ -0,0 +1,52 @@
+import { useStore } from "@nanostores/react";
+import { isCartOpen } from "../../globaStore/ui";
+// import { addCartItem, cartItems } from "./store/gameState";
+
+const ReactGame = () => {
+  const $isCartOpen = useStore(isCartOpen);
+  // const $cartItems = useStore(cartItems);
+  // const hardCodedItemInfo = {
+  //   id: "id",
+  //   name: "Product name",
+  //   imageSrc: "img-src.pgn",
+  // };
+
+  // function addToCart() {
+  //   isCartOpen.set(true);
+  //   addCartItem(hardCodedItemInfo);
+  // }
+
+  return (
+    <div
+      style={{
+        background: "#2c2c2c",
+        color: "white",
+        padding: 12,
+      }}
+    >
+      {/* {JSON.stringify($cartItems)} */}
+      <h1
+        style={{
+          marginTop: "0",
+          textShadow: "2px 2px 2px black",
+        }}
+      >
+        React component
+      </h1>
+      <button
+        style={{
+          color: "#2c2c2c",
+          background: "white",
+          padding: "6px 12px",
+          boxShadow: "2px 2px 2px black",
+        }}
+        onClick={() => isCartOpen.set(!$isCartOpen)}
+      >
+        Visa vue component
+      </button>
+      {/* <button onClick={addToCart}>add to cart</button> */}
+    </div>
+  );
+};
+
+export { ReactGame };
---
+++ src/components/menu.vue
@@ -0,0 +1,12 @@
+<template>
+  <!--write to the imported store using `.set`-->
+  <p v-if="$isCartOpen">Hej from a Vue component</p>
+</template>
+
+<script lang="ts" setup>
+import { useStore } from "@nanostores/vue";
+import { isCartOpen } from "../globaStore/ui";
+
+// read the store value with the `useStore` hook
+const $isCartOpen = useStore(isCartOpen);
+</script>
